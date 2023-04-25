47c2278759ef9e2fe6f19960fb4b2326174be93c
Carolina <carolina.palacios1995@gmail.com>
2023-04-25 14:15:28 -0400

    created main menu questions prompt

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const query = require("./query.js");
 
+// Inquirer questions menu
+const init = async () => {
+    while (true) {
+        // launches the prompt interface (inquiry session)
+        const choice = await inquirer.prompt({
+            type: "list",
+            name: "menu",
+            message: "What would you like to do?",
+            choices: [
+                "View all Departments",
+                "View all Roles",
+                "View all Employees",
+                "Add a new Department",
+                "Add a new Role",
+                "Add a new Employee",
+                "Update Employee Role",
+                "Exit"
+            ],
+        });
 
+    };
+};
 
 // A Function call to initialize app
 init();
