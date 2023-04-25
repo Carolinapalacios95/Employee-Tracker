0f6e629c36cedb3ad55db6142b11d82e33ebd3c6
Carolina <carolina.palacios1995@gmail.com>
2023-04-25 11:34:49 -0400

    added get all employees function

--- server.js
+++ server.js
@@ -30,3 +30,18 @@ const getAllRoles = async () => {
          ORDER BY `roles`.`id` ASC");
     return data[0];
 };
+
+// Get employee, role and department data from database
+const getAllEmployees = async () => {
+    const data = await db.promise().execute(
+        "SELECT `e`.`id`, `e`.`first_name`, `e`.`last_name`, `r`.`title`, `d`.`name` AS `departments`, `r`.`salary`, CONCAT(`m`.`first_name`, ' ', `m`.`last_name`) AS `manager`\
+         FROM `employees` e\
+         JOIN `roles` r\
+         ON `r`.`id` = `e`.`role_id`\
+         JOIN `departments` d\
+         ON  `r`.`department_id` = `d`.`id`\
+         LEFT JOIN `employees` m\
+         ON `e`.`manager_id` = `m`.`id`\
+         ORDER BY `e`.`id` ASC");
+    return data[0];
+};
