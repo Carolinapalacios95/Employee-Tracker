42ca60a0202f67539a0b65e1363647359fb82c69
Carolina <carolina.palacios1995@gmail.com>
2023-04-25 11:22:43 -0400

    added get all roles function

--- server.js
+++ server.js
@@ -12,11 +12,21 @@ const db = mysql.createConnection(
       database: 'employee_db'
     },
     console.log(`Connected to the classlist_db database.`)
-  );
+);
 
 // Gets department data from database
 const getAllDepartments = async () => {
     const data = await db.promise().execute("SELECT * FROM `departments`");
     return data[0];
-}
+};
 
+// Gets roles and department data from database
+const getAllRoles = async () => {
+    const data = await db.promise().execute(
+        "SELECT `roles`.`id`, `roles`.`title`, `departments`.`name` AS `departments`, `roles`.`salary`\
+         FROM `roles`\
+         JOIN `departments`\
+         ON `roles`.`department_id` = `departments`.`id`\
+         ORDER BY `roles`.`id` ASC");
+    return data[0];
+};
