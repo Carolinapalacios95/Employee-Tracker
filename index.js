32da1fc1f359a8684ec3c8d48ef94d2f4e1e08c3
Carolina <carolina.palacios1995@gmail.com>
2023-04-25 15:09:43 -0400

    added else statements for add new dept & role

--- index.js
+++ index.js
@@ -38,9 +38,55 @@ const init = async () => {
             const data = await query.getAllEmployees();
             console.table(data);
 
-        
+        } else if (choice.menu === "Add a new Department") {
+            // runs a new prompt
+            const newDept = await inquirer.prompt({
+                type: "input",
+                name: "name",
+                message: "Please enter a department name.",
+                validate: (input) => {
+                //checks to make sure new dept name is within the VARCHAR(30) character limit
+                    return input.length <= 30;
+                },
+            });
+            //passes the user input as parameters into the addDepartment function in server.js
+            await query.addDepartment(newDept.name);
+            console.log("New department successfully added.");
 
-        }
+        } else if (choice.menu === "Add a new Role") {
+            // Creates a const that will hold the departments array for user to select from in prompt
+            const dept_list = await query.getAllDepartments();
+            const dept_choices = dept_list.map(({ id, name }) => ({
+                name: name,
+                value: id,
+            }));
+            // runs a new prompt
+            const newRole = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "title",
+                    message: "Please enter a title for the new role.",
+                    validate: (input) => {
+                        return input.length <= 30;
+                    },
+                },
+                {
+                    type: "input",
+                    name: "salary",
+                    message: "Please enter a salary amount for the new role.",
+                },
+                {
+                    type: "list",
+                    name: "dept",
+                    message: "Please select the department you would like to add this role to.",
+                    choices: dept_choices,
+                },
+            ]);
+            //passes the user input as parameters into the addRole function in server.js
+            await query.addRole(newRole.title, newRole.salary, newRole.dept);
+            console.log("New role successfully added.");
+
+        } 
 
     };
 };
