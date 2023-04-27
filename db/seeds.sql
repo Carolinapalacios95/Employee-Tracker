26b011556da33f2f8a7d69a52b638c10a3c8cf30
Carolina <carolina.palacios1995@gmail.com>
2023-04-26 20:47:22 -0400

    debugged code

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,8 +5,8 @@ VALUES  ("Sales"),
         ("Engineering");
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ("Sales Lead", 10000, 1),
-        ("Salesperson", 8000, 1),
+VALUES  ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
         ("Lead Engineer", 150000, 4),
         ("Software Engineer", 120000, 4),
         ("Account Manager", 160000, 3),
